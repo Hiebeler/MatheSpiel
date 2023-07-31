5540281ecaf41a53335a46fdfef5fcc344452c98
Emanuel Hiebeler <emanuel.hiebeler@gmail.com>
2023-07-31 20:46:19 +0200

    some changes

--- Node2D.gd
+++ Node2D.gd
@@ -4,10 +4,10 @@ var is_dragging = false
 var touchpos = 0
 @onready var sprite_2d = $Sprite2D
 
-func _input(event):
+func _unhandled_input(event):
 	var local_event = make_input_local(event)
 	
-	if local_event is InputEventMouseButton:
+	if local_event is InputEventScreenTouch:
 		if local_event.is_pressed():
 			is_dragging=true
 		else:
